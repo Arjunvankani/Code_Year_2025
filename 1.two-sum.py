75eb353f64a1e3c5d57937d55a9c8b5f77713d92
Vankani Arjun <65284186+Arjunvankani@users.noreply.github.com>
2025-01-01 10:39:06 +0000

    Day 1

---
+++ 1.two-sum.py
@@ -0,0 +1,23 @@
+#
+# @lc app=leetcode id=1 lang=python
+#
+# [1] Two Sum
+#
+
+# @lc code=start
+class Solution(object):
+    def twoSum( nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        for i in range(len(nums)):
+            for j in range(i+1, len(nums)):
+                #print(i,j)
+                if nums[j] == target - nums[i]:
+                    return [i,j]
+            return []
+    #print(twoSum(nums=[2,7,11,15],target=9))       
+# @lc code=end
+
